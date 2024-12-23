262d50220b724502450ffc954ef19ce61c4edd3d
balogun14 <awwalbalogun06@gmail.com>
2024-12-23 11:27:00 +0100

    `Refactor manipulateString.js: replace multiple passes with a single pass`

--- src/lib/string-manipulation/manipulateString.js
+++ src/lib/string-manipulation/manipulateString.js
@@ -2,23 +2,19 @@ function manipulateString(input) {
     if (typeof input !== 'string') {
         throw new Error('Input must be a string');
     }
-
-    // Convert all lowercase letters to uppercase and vice versa
-    let swappedCaseString = input.split('').map(char => {
-        if (char === char.toLowerCase()) {
-            return char.toUpperCase();
-        } else if (char === char.toUpperCase()) {
-            return char.toLowerCase();
-        }
-        else {
-            return char;
-        }
-    }).join('');
-
-    // Remove all vowels from the string
-    let resultString = swappedCaseString.replace(/[aeiouAEIOU]/g, '');
-
-    return resultString;
+    // Single pass through the string
+    return input.split('')
+        .map(char => {
+            // Skip vowels
+            if (/[aeiouAEIOU]/.test(char)) {
+                return '';
+            }
+            // Swap case for non-vowels
+            return char === char.toLowerCase() ? 
+                   char.toUpperCase() : 
+                   char.toLowerCase();
+        })
+        .join('');
 }
 
 module.exports = { manipulateString };
